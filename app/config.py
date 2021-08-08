3e0ce0fd97a2acc37fb1931f24ec530978efb5c0
Johnny Whitworth <johnny.whitworth@azp.local>
2021-08-08 11:58:30 -0700

    added email response after submission

--- app/config.py
+++ app/config.py
@@ -7,13 +7,20 @@ ERP_HOST = os.getenv('ERP_HOST')
 ERP_UID = os.getenv('ERP_UID')
 ERP_PWD = os.getenv('ERP_PWD')
 
-FTP_SERVER=os.getenv('FTP_SERVER')
-FTP_PORT=int(os.getenv('FTP_PORT'))
-FTP_USR=os.getenv('FTP_USR')
-FTP_PWD=os.getenv('FTP_PWD')
-FTP_PATH=os.getenv('FTP_PATH')
+FTP_SERVER = os.getenv('FTP_SERVER')
+FTP_PORT = int(os.getenv('FTP_PORT'))
+FTP_USR = os.getenv('FTP_USR')
+FTP_PWD = os.getenv('FTP_PWD')
+FTP_PATH = os.getenv('FTP_PATH')
 
-GPG_PATH='C:/Apps/GnuPG/gpg.exe'
-GPG_HOME='C:/Apps/voya/pgp'
-GPG_KEY=os.getenv('GPG_KEY')
-GPG_FINGERPRINT=os.getenv('GPG_FINGERPRINT')
+GPG_PATH = 'C:/Apps/GnuPG/gpg.exe'
+GPG_HOME = 'C:/Apps/voya/pgp'
+GPG_KEY = os.getenv('GPG_KEY')
+GPG_FINGERPRINT = os.getenv('GPG_FINGERPRINT')
+
+EMAIL_UID = os.getenv('EMAIL_UID')
+EMAIL_PWD = os.getenv('EMAIL_PWD')
+EMAIL_SMTP = os.getenv('EMAIL_SMTP')
+EMAIL_SMTP_PORT = int(os.getenv('EMAIL_SMTP_PORT'))
+
+DUMP_LOCALE = os.getenv('DUMP_LOCALE')
--- main.py
+++ main.py
@@ -1,9 +1,27 @@
 from app import collect_voya_data, send_to_sftp, pgp_encryption, clean_up
-
+from app.config import DUMP_LOCALE
+from app.email import send_email
 
 if __name__ == "__main__":
-    file = collect_voya_data()
-    pgp_encryption(file)
-    send_to_sftp(file)
-    clean_up(True)
+    try:
+        file = collect_voya_data()
+        response = f'Data Collected: Filename {file}\n'
+        try:
+            pgp_encryption(file)
+            response += f'Encrption Successful\n'
+            try: 
+                send_to_sftp(file)
+                response += f'File Tranmission Successful\n'
+                clean_up(True)
+                send_email('Voya Tranmission', response)
+            except Exception as e:
+                response = f'Transmission failed with error: {e}'
+        except Exception as e:
+            response = f'Encryption failed with error: {e}'
+    except Exception as e:
+        resposne = f'File collection failed with error: {e}'
+
+    # pgp_encryption(file)
+    # send_to_sftp(file)
+    # clean_up(True)
 
